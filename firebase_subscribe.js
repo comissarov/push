e5a50a58afd81e8eb2b6d531321e917d425f06e4
Max <32263474+comissarov@users.noreply.github.com>
2018-06-18 16:05:42 +0300

    Update firebase_subscribe.js

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -32,7 +32,7 @@ if (
         console.log('Message received', payload);
 
         // register fake ServiceWorker for show notification on mobile devices
-        navigator.serviceWorker.register('/serviceworker/firebase-messaging-sw.js');
+        navigator.serviceWorker.register('/push/firebase-messaging-sw.js');
         Notification.requestPermission(function(permission) {
             if (permission === 'granted') {
                 navigator.serviceWorker.ready.then(function(registration) {
